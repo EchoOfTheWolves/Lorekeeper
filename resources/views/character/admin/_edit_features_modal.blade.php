b21ac4eea17b9de6e9d03fd028c11aacfc73ed69
EchoOfTheWolves <vickysun2004@gmail.com>
2025-01-14 14:46:54 +1100

    no message

--- resources/views/character/admin/_edit_features_modal.blade.php
+++ resources/views/character/admin/_edit_features_modal.blade.php
@@ -42,11 +42,11 @@
         @endforeach
     </div>
     <div><a href="#" class="btn btn-primary" id="add-feature">Add Trait</a></div>
-    <div class="feature-row hide mb-2">
-        {!! Form::select('feature_id[]', $features, null, ['class' => 'form-control mr-2 feature-select', 'placeholder' => 'Select Trait']) !!}
-        {!! Form::text('feature_data[]', null, ['class' => 'form-control mr-2', 'placeholder' => 'Extra Info (Optional)']) !!}
-        <a href="#" class="remove-feature btn btn-danger mb-2">×</a>
-    </div>
+        <div class="feature-row hide mb-2">
+            {!! Form::select('feature_id[]', $features, null, ['class' => 'form-control mr-2 feature-select', 'placeholder' => 'Select Trait']) !!}
+            {!! Form::text('feature_data[]', null, ['class' => 'form-control mr-2', 'placeholder' => 'Extra Info (Optional)']) !!}
+            <a href="#" class="remove-feature btn btn-danger mb-2">×</a>
+        </div>
 </div>
 
 <div class="text-right">

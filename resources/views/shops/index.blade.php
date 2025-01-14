0698d8be31c3670f520ec52f2d99cb57b8ed0c54
EchoOfTheWolves <vickysun2004@gmail.com>
2025-01-15 09:57:13 +1100

    no message

--- resources/views/shops/index.blade.php
+++ resources/views/shops/index.blade.php
@@ -10,27 +10,6 @@
 </h1>
 
 <div class="row shops-row">
-    @foreach($shops as $shop)
-        @if($shop->visible_only == 1)
-            <div class="col-md-3 col-6 mb-3 text-center collectionnotunlocked">
-                <div class="shop-image">
-                    <a href="{{ $shop->url }}"><img src="{{ $shop->shopImageUrl }}" alt="{{ $shop->name }}" /></a>
-                </div>
-                <div class="shop-name mt-1">
-                    <a href="{{ $shop->url }}" class="h5 mb-0">{{ $shop->name }} <i class="fas fa-eye" data-toggle="tooltip" title="View-only"></i> </a> 
-                </div>
-            </div>
-        @else
-            <div class="col-md-3 col-6 mb-3 text-center">
-                <div class="shop-image">
-                    <a href="{{ $shop->url }}"><img src="{{ $shop->shopImageUrl }}" alt="{{ $shop->name }}" /></a>
-                </div>
-                <div class="shop-name mt-1">
-                    <a href="{{ $shop->url }}" class="h5 mb-0">{{ $shop->name }}</a>
-                </div>
-            </div>
-        @endif
-    @endforeach
 @if($shops->count())
 @foreach($shops as $categoryId=>$categoryshops)
 <div class="col-md-12">
@@ -65,6 +44,28 @@
         </div>
 @endforeach
 @endif
+
+@foreach($shops as $shop)
+        @if($shop->visible_only == 1)
+            <div class="col-md-3 col-6 mb-3 text-center collectionnotunlocked">
+                <div class="shop-image">
+                    <a href="{{ $shop->url }}"><img src="{{ $shop->shopImageUrl }}" alt="{{ $shop->name }}" /></a>
+                </div>
+                <div class="shop-name mt-1">
+                    <a href="{{ $shop->url }}" class="h5 mb-0">{{ $shop->name }} <i class="fas fa-eye" data-toggle="tooltip" title="View-only"></i> </a> 
+                </div>
+            </div>
+        @else
+            <div class="col-md-3 col-6 mb-3 text-center">
+                <div class="shop-image">
+                    <a href="{{ $shop->url }}"><img src="{{ $shop->shopImageUrl }}" alt="{{ $shop->name }}" /></a>
+                </div>
+                <div class="shop-name mt-1">
+                    <a href="{{ $shop->url }}" class="h5 mb-0">{{ $shop->name }}</a>
+                </div>
+            </div>
+        @endif
+    @endforeach
 </div>
 
 @endsection

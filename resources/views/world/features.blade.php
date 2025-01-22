bf7e361e3e11c5f25065bcc70df541cb2a8f37b0
EchoOfTheWolves <vickysun2004@gmail.com>
2025-01-22 11:35:00 +1100

    no message

--- resources/views/world/features.blade.php
+++ resources/views/world/features.blade.php
@@ -15,9 +15,6 @@
             <div class="form-group ml-3 mb-3">
                 {!! Form::select('species_id', $specieses, Request::get('species_id'), ['class' => 'form-control']) !!}
             </div>
-            <div class="form-group mr-3 mb-3">
-            {!! Form::select('subtype_id', $subtype, Request::get('subtype_id'), ['class' => 'form-control']) !!}
-        </div>
             <div class="form-group ml-3 mb-3">
                 {!! Form::select('rarity_id', $rarities, Request::get('rarity_id'), ['class' => 'form-control']) !!}
             </div>

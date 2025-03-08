4d1ce32c0bc59b91f58ea27de528f12b3ec52389
EchoOfTheWolves <vickysun2004@gmail.com>
2025-03-08 11:48:25 +1100

    testing higher lower

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('check-sales')
                 ->everyMinute();
         $schedule->command('reset-hol')
-                 ->daily();
+                 ->everyminute();
         $schedule->command('update-timed-daily')
                 ->everyMinute();          
 

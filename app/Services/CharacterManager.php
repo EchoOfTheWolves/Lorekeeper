3d984376f6c649f47b9a939304382713457fcb28
EchoOfTheWolves <vickysun2004@gmail.com>
2025-03-22 14:14:03 +1100

    revert

--- app/Services/CharacterManager.php
+++ app/Services/CharacterManager.php
@@ -2138,7 +2138,7 @@ is_object($sender) ? $sender->id : null,
             $species = ($request->character->is_myo_slot && $request->character->image->species_id) ? $request->character->image->species : Species::find($data['species_id']);
 
             if (($request->character->is_myo_slot && count($request->character->image->subtypes))) {
-                $subtypes = $request->character->image->subtypes()->pluck('subtype_ids')->toArray();
+                $subtypes = $request->character->image->subtypes()->pluck('subtype_id')->toArray();
             } else {
                 if(isset($data['subtype_ids']) && $data['subtype_ids']) {
                     $subtypes = $data['subtype_ids'];

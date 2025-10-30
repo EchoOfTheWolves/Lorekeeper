87c29684b37459c4ee112f8bd279b467d6ac7d43
VICKYSPC\josep <vickysun2004@gmail.com>
2025-10-30 19:20:42 +1100

    no message

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -10,10 +10,8 @@
 @endsection
 
 @section('profile-content')
-    {!! breadcrumbs(['Users' => 'users', $user->name => $user->url, $user->userBorder()]) !!}
-<div style=" float:left;">
-            {!! $user->userBorder() !!}
-        </div>
+    {!! breadcrumbs(['Users' => 'users', $user->name => $user->url]) !!}
+
     @if (mb_strtolower($user->name) != mb_strtolower($name))
         <div class="alert alert-info">This user has changed their name to <strong>{{ $user->name }}</strong>.</div>
     @endif

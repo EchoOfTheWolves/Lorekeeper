aafd791f7e15d3aa1b600ee8e850fa10caccd318
VICKYSPC\josep <vickysun2004@gmail.com>
2026-05-05 18:15:49 +1000

    no message

--- app/Services/Item/BoxService.php
+++ app/Services/Item/BoxService.php
@@ -28,7 +28,11 @@ class BoxService extends Service {
      */
     public function getEditData() {
         return [
-            
+            'characterCurrencies' => Currency::where('is_character_owned', 1)->orderBy('sort_character', 'DESC')->pluck('name', 'id'),
+            'items' => Item::orderBy('name')->pluck('name', 'id'),
+            'currencies' => Currency::where('is_user_owned', 1)->orderBy('name')->pluck('name', 'id'),
+            'tables' => LootTable::orderBy('name')->pluck('name', 'id'),
+            'raffles' => Raffle::where('rolled_at', null)->where('is_active', 1)->orderBy('name')->pluck('name', 'id'),
         ];
     }
 
